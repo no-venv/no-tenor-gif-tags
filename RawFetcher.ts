1ec04e87b6eed73d1c864b46130a21b58c2e430b
no-venv <>
2025-07-08 12:56:12 -0600

    remove unused code

--- RawFetcher.ts
+++ RawFetcher.ts
@@ -14,17 +14,4 @@ export function get_raw_gif(url: string) {
     const gif_id = regex_match[1];
     const compiled_gif_link = `https://tenor.com/view/${gif_id}`;
     return compiled_gif_link;
-    // return fetch(url).then(function (result) {
-    //     return result.text();
-    // })
-    //     .then(function (tenor_html_page) {
-    //         const regex_match = tenor_html_page.match(/((media1.tenor.com)\/m\/.*?)\//g);
-    //         if (!regex_match) {
-    //             // return error
-    //             return;
-    //         }
-    //         const gif_id = regex_match[0].split("/")[2];
-    //         const compiled_gif_link = `https://c.tenor.com/${gif_id}/tenor.gif`;
-    //         return compiled_gif_link;
-    //     });
 }
